f65eed95817cb44fa045c96990a1a51425ec392e
Tyler Sell <tylerselldesign@gmail.com>
2019-10-07 13:28:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,12 @@ class ApplicationController < Sinatra::Base
   
   helpers do 
   
-    def is_logged_in?
-      !!session[:user_id]
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
     
   end
