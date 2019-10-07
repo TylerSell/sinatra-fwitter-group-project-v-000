dcfe34ba9a0b55ce7a8742b51a9c1c5b82d5f517
Tyler Sell <tylerselldesign@gmail.com>
2019-10-07 13:31:12 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,18 +19,3 @@ class User < ActiveRecord::Base
 
 end
 
-class Helpers < ActiveRecord::Base
-
-  def self.current_user(user)
-    @user = User.find_by(id: user[:user_id])
-  end
-
-  def self.is_logged_in?(user)
-    if user[:user_id]
-      true
-    else
-      false
-    end
-  end
-
-end
